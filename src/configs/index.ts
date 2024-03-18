564dbddf121353a9a873bc9e415dda20192953a1
João Pedro R. Diniz <jp_rd@hotmail.com>
2024-03-18 17:50:47 -0300

    Ícones adicionados, arquivo de configuração atualizado e responsividade aplicada

---
+++ src/configs/index.ts
@@ -0,0 +1,36 @@
+export enum Language {
+	java = 'java',
+	javascript = 'javascript',
+	python = 'python',
+}
+
+export default interface ProjectModel {
+	name: string
+	repository: string
+	description: string
+	project?: string
+}
+
+export const projects: { [key in Language]: ProjectModel[] } = {
+	java: [
+		{
+			name: "Profanator-API",
+			repository: "https://github.com/J-Player/Profanator-API",
+			description:
+				"API reativa desenvolvida em Spring WebFlux com o propósito de fornecer dados relacionados aos itens do Profane (MMO Sandbox brasileiro).",
+				project: 'https://profanator-api.onrender.com/swagger-ui/index.html'
+		},
+		{
+			name: "Simple Spring Batch Template",
+			repository: "https://github.com/J-Player/Simple-Spring-Batch-Template",
+			description: "Um simples template de um projeto usando Spring Batch para processamento de lotes.",
+		},
+		{
+			name: "Simple Spring Kafka Template",
+			repository: "https://github.com/J-Player/Simple-Spring-Kafka-Template",
+			description: "Um simples template de uma mensageria usando Spring Kafka.",
+		},
+	],
+	javascript: [],
+	python: [],
+}
---
+++ src/styles/breakpoints.ts
@@ -0,0 +1,17 @@
+enum sizes {
+	xs = "400px",
+	sm = "600px",
+	md = "768px", //Largura máxima do meu celular (deitado)
+	lg = "992px",
+	xl = "1366px",
+}
+
+const breakpoints = {
+	xs: `(max-width: ${sizes.xs})`,
+	sm: `(max-width: ${sizes.sm})`,
+	md: `(max-width: ${sizes.md})`,
+	lg: `(max-width: ${sizes.lg})`,
+	xl: `(min-width: ${sizes.xl})`,
+}
+
+export default breakpoints
